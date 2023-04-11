bb4e2e1f08298c04f406d1a4cd1ee93dc5b779b9
Tobias Eriksen <te@mindthemedia.com>
2023-04-11 22:31:58 +0200

    update

---
+++ components/item/Pill.vue
@@ -0,0 +1,3 @@
+<template>
+	<label class="absolute top-4 right-4 text-[12px] pointer-events-none text-white z-10 opacity-50 bg-black/25 py-1 px-3 rounded-full"><slot/></label>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<AppHeader />
 	<div class="grid grid-cols-12 h-[calc(100vh-64px)] justify-between">
-		<div v-if="showToggleSwitches" class="flex absolute z-10 bottom-8 left-1/2 -translate-x-1/2 -translate-y-1/2 p-2 rounded-full bg-white space-x-2">
+		<div v-if="showToggleSwitches" class="flex absolute z-10 bottom-4 sm:bottom-8 left-1/2 -translate-x-1/2 lg:-translate-y-1/2 p-2 rounded-full bg-white space-x-2">
 			<div :class="htmlIsValid ? 'bg-[#2ad42a]' : 'bg-[#e33454]'" class="grid place-items-center w-[48px] h-[48px] rounded-full text-white">
 				<svg v-if="htmlIsValid" class="w-[24px] fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
 				<svg v-else class="w-[24px] fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>
@@ -13,17 +13,17 @@
 			</button>
 		</div>
 		<div class="col-span-12 sm:col-span-6 lg:col-span-4 relative">
-			<label class="absolute top-4 right-4 text-[12px] pointer-events-none text-white z-10 opacity-50">HTML</label>
-			<textarea spellcheck="false" class="absolute inset-0 w-full h-full outline-none p-8 bg-[#111827] text-white" v-model="html" placeholder="Write some HTML here..." />
+			<ItemPill>HTML</ItemPill>
+			<textarea spellcheck="false" class="relative sm:absolute inset-0 w-full h-full outline-none p-8 bg-[#111827] text-white" v-model="html" placeholder="Write some HTML here..." />
 		</div>
 		<div class="col-span-12 sm:col-span-6 lg:col-span-4 h-full overflow-y-auto bg-white/5 relative">
-			<label class="absolute top-4 right-4 text-[12px] pointer-events-none text-white z-10 opacity-50">JSON</label>
+			<ItemPill>JSON</ItemPill>
 			<client-only>
 				<pre v-if="activeShow === 'output'" class="p-8 text-[14px]">{{theHTML}}</pre>
 			</client-only>
 		</div>
 		<div class="col-span-12 lg:col-span-4 h-full overflow-y-auto bg-white/10 relative">
-			<label class="absolute top-4 right-4 text-[12px] pointer-events-none text-white z-10 opacity-50">OUTPUT</label>
+			<ItemPill>OUTPUT</ItemPill>
 			<client-only>
 				<RichTextToHtml v-if="theHTML" class="p-8 prose dark:prose-invert" :json="theHTML" />
 			</client-only>
